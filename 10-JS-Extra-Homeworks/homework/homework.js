a6b56560e59b7bb0e5042057a52cc19e5aa63e3d
Jean Paul <paul.fa.ac@gmail.com>
2021-11-26 23:16:15 -0500

    Homework 10-JS-extra solved

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -1,86 +1,160 @@
 // No cambies los nombres de las funciones.
 
-function deObjetoAmatriz(objeto){
-  // Escribe una función que convierta un objeto en una matriz, donde cada elemento representa 
+function deObjetoAmatriz(objeto) {
+  // Escribe una función que convierta un objeto en una matriz, donde cada elemento representa
   // un par clave-valor en forma de matriz.
-  //Ejemplo: 
+  //Ejemplo:
   /*objeto({
       D: 1,
       B: 2,
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var matriz = [];
+  for (var clave in objeto) {
+    matriz.push([clave, objeto[clave]]);
+  }
+  return matriz;
 }
-
 
 function numberOfCharacters(string) {
-  //La función recibe un string. Recorre el srting y devuelve el caracter con el número de veces que aparece 
+  //La función recibe un string. Recorre el srting y devuelve el caracter con el número de veces que aparece
   //en formato par clave-valor.
-  //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
+  //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
   //Escribe tu código aquí
+  var letras = {};
+  for (var i = 0; i < string.length; i++) {
+    if (letras[string[i]] === undefined) {
+      letras[string[i]] = 1;
+    } else {
+      letras[string[i]]++;
+    }
+  }
+  return letras;
 }
-
 
 function capToFront(s) {
   //Realiza una función que reciba como parámetro un string y mueva todas las letras mayúsculas
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayus = "";
+  var minus = "";
+  for (i = 0; i < s.length; i++) {
+    if (
+      (s[i].codePointAt() >= 97 && s[i].codePointAt() <= 122) ||
+      s[i].codePointAt() === 241
+    ) {
+      minus += s[i];
+    }
+    if (
+      (s[i].codePointAt() >= 65 && s[i].codePointAt() <= 90) ||
+      s[i].codePointAt() === 209
+    ) {
+      mayus += s[i];
+    }
+  }
+  return mayus + minus;
 }
 
-
 function asAmirror(str) {
-  //La función recibe una frase. 
-  //Escribe una función que tome la frase recibida y la devuelva de modo tal que se pueda leer de izquierda a derecha 
+  //La función recibe una frase.
+  //Escribe una función que tome la frase recibida y la devuelva de modo tal que se pueda leer de izquierda a derecha
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
-} 
+  var newPhase = "";
+  var reverse = "";
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === " ") {
+      newPhase = newPhase + reverse + str[i];
+      reverse = "";
+    } else {
+      reverse = str[i] + reverse;
+      if (i === str.length - 1) {
+        newPhase = newPhase + reverse;
+      }
+    }
+  }
+  return newPhase;
+}
 
-
-function capicua(numero){
+function capicua(numero) {
   //Escribe una función, la cual recibe un número y determina si es o no capicúa.
-  //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
+  //La misma debe retornar: "Es capicua" si el número se número que se lee igual de
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  function reverseNumber(numero) {
+    var reverse = 0;
+    while (numero > 0) {
+      reverse = reverse * 10 + (numero % 10);
+      numero = Math.floor(numero / 10);
+    }
+    return reverse;
+  }
+
+  return numero === reverseNumber(numero) ? "Es capicua" : "No es capicua";
 }
 
-
-function deleteAbc(cadena){
-  //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
+function deleteAbc(cadena) {
+  //Define una función que elimine las letras "a", "b" y "c" de la cadena dada
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var newCadena = "";
+  for (var i = 0; i < cadena.length; i++) {
+    if (
+      cadena[i] !== "a" &&
+      cadena[i] !== "b" &&
+      cadena[i] !== "c" &&
+      cadena[i] !== "A" &&
+      cadena[i] !== "B" &&
+      cadena[i] !== "C"
+    ) {
+      newCadena += cadena[i];
+    }
+  }
+  return newCadena;
 }
-
 
 function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  var newSort = arr.sort((a, b) => {
+    return a.length - b.length;
+  });
+  return newSort;
 }
 
-
-function buscoInterseccion(arreglo1, arreglo2){
-  //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
+function buscoInterseccion(arreglo1, arreglo2) {
+  //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  var inter = [];
+
+  for (var i = 0; i < arreglo1.length; i++) {
+    for (var j = 0; j < arreglo2.length; j++) {
+      if (arreglo1[i] === arreglo2[j]) {
+        inter.push(arreglo2[j]);
+        break;
+      }
+    }
+  }
+  return inter;
 }
-
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
 
 module.exports = {
-   deObjetoAmatriz,
-   numberOfCharacters,
-   capToFront,
-   asAmirror,
-   capicua,
-   deleteAbc,
-   sortArray,
-   buscoInterseccion,
+  deObjetoAmatriz,
+  numberOfCharacters,
+  capToFront,
+  asAmirror,
+  capicua,
+  deleteAbc,
+  sortArray,
+  buscoInterseccion
 };
-
